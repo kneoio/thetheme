1e2a2978beaa3c5a7750a0c8b5a3aa13b1ffe9a9
kaira <justaidajam@gmail.com>
2025-12-03 19:17:40 +0000

    #

--- keycloak/themes/kneo/login/resources/js/theme.js
+++ keycloak/themes/kneo/login/resources/js/theme.js
@@ -1,19 +1,2 @@
-(function(){
-  function getCookie(name){
-    var value = '; ' + document.cookie;
-    var parts = value.split('; ' + name + '=');
-    if(parts.length === 2) return parts.pop().split(';').shift();
-    return null;
-  }
-  var theme = getCookie('theme');
-  if(theme === 'dark'){
-    var html = document.documentElement;
-    if(html.className.indexOf('kneo-dark') === -1){
-      html.className += (html.className ? ' ' : '') + 'kneo-dark';
-    }
-    var link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = 'resources/css/dark.css';
-    document.head.appendChild(link);
-  }
-})();
+// Dark theme is now loaded via dark-override.css in theme.properties
+// No JavaScript needed
